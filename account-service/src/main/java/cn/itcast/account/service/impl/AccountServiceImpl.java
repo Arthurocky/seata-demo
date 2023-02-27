955d81496588e69044c4d23f1c03952db1433efd
Athurocky <arthurocky@user.noreply.gitee.com>
2023-02-27 21:49:55 +0800

    🎉 Base Setup

--- account-service/src/main/java/cn/itcast/account/service/impl/AccountServiceImpl.java
+++ account-service/src/main/java/cn/itcast/account/service/impl/AccountServiceImpl.java
@@ -6,13 +6,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import javax.annotation.Resource;
 
 
 @Slf4j
 @Service
 public class AccountServiceImpl implements AccountService {
 
-    @Autowired
+    @Resource
     private AccountMapper accountMapper;
 
     @Override
